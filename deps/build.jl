1946fb6fb2efce0533b2d09efb7dc107db175545
John Lapeyre <john.lapeyre@gmail.com>
2014-12-15 03:10:50 +0100

    Initial non-empty commit 	new file:   .gitignore 	modified:   .travis.yml 	new file:   REQUIRE 	new file:   deps/.gitignore 	new file:   deps/build.jl 	modified:   src/PrimeSieve.jl

---
+++ deps/build.jl
@@ -0,0 +1,23 @@
+using BinDeps
+
+@BinDeps.setup
+
+primesieve = library_dependency("primesieve", aliases = ["libprimesieve"])
+
+provides(Sources,URI("http://dl.bintray.com/kimwalisch/primesieve/primesieve-5.4.1.tar.gz"),primesieve)
+provides(BuildProcess,Autotools(libtarget = ".libs/libprimesieve."*BinDeps.shlib_ext),primesieve)
+
+# The windows and osx binaries do contain the library, only the executable.
+
+#@windows_only begin
+#    provides(Binaries, {URI("http://dl.bintray.com/kimwalisch/primesieve/primesieve-5.4-win$WORD_SIZE.zip") => primesieve}, os = :Windows )
+#end
+
+#@osx_only begin
+#    if Pkg.installed("Homebrew") === nothing
+#        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")  end
+#    using Homebrew
+#    provides( Homebrew.HB, "zeromq32", primesieve, os = :Darwin )
+#end
+
+@BinDeps.install [:primesieve => :primesieve]
--- src/PrimeSieve.jl
+++ src/PrimeSieve.jl
@@ -1,5 +1,78 @@
 module PrimeSieve
 
-# package code goes here
+include("../deps/deps.jl")
+import Base: convert, ccall
 
-end # module
+export countprimes, primes, nprimes, nthprime, pnthprime, primetest
+
+##
+
+# Returned primes have this data type
+const LONG_PRIMES = 4
+const ULONG_PRIMES = 5
+
+const libname = "primesieve/.libs/libprimesieve.so.4"
+
+# Copy the returned array, and free C array
+function primescopy(res,n)
+    retv = pointer_to_array(res,n,false)
+    nretv = copy(retv)
+    ccall((:primesieve_free, libname), Void, (Ptr{Void},), res)
+    nretv
+end
+
+function countprimes{T,V}(start::T, stop::V)
+    (start,stop) = promote(start,stop)
+    res = ccall((:primesieve_count_primes,libname),
+                Ptr{Uint64}, (Uint64, Uint64),
+                convert(Uint64,start), convert(Uint64,stop))
+    convert(typeof(start),res)
+end
+
+countprimes(stop) = countprimes(one(typeof(stop)),stop)
+
+primetype(::Type{Int64}) = LONG_PRIMES
+primetype(::Type{Uint64}) = ULONG_PRIMES
+
+function primes{T,V}(start::T,stop::V)
+    (start,stop) = promote(start,stop)
+    NT = typeof(start)
+    n = Csize_t[0]
+    res = ccall((:primesieve_generate_primes, libname),
+                Ptr{T}, (Uint64, Uint64, Ptr{Csize_t}, Int),
+                convert(Uint64,start),convert(Uint64,stop),n,primetype(typeof(start)))
+    primescopy(res,n[1])
+end
+
+primes(stop) = primes(one(typeof(stop)),stop)
+
+function nprimes(nin,start)
+    n = convert(Int,nin)
+    res = ccall((:primesieve_generate_n_primes, libname),
+                Ptr{Int64}, (Uint64, Uint64, Int),
+                convert(Uint64,n),convert(Uint64,start),LONG_PRIMES)
+    primescopy(res,n)
+end
+
+nprimes(n) = nprimes(n,one(typeof(n)))
+
+#  @param n  if n = 0 finds the 1st prime >= start,
+#            if n > 0 finds the nth prime > start,
+#            if n < 0 finds the nth prime < start (backwards).
+#  @pre   start <= 2^64 - 2^32 * 11.
+for (cname,jname) in ((:(:primesieve_nth_prime), :nthprime),
+                      (:(:primesieve_parallel_nth_prime), :pnthprime))
+    @eval begin
+        function ($jname)(n,start)
+            res = ccall(( $cname, libname),
+                         Ptr{Uint64}, (Int64, Uint64),
+                        convert(Uint64,n),convert(Int64,start))
+            convert(Int,res)
+        end
+        ($jname)(n) = ($jname)(n,one(typeof(n)))        
+    end
+end
+
+primetest() = ccall((:primesieve_test, libname), Void, ())
+
+end # module PrimeSieve
