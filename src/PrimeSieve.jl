db89ff98c56ba4568145110b32986f6dc19692e3
John Lapeyre <john.lapeyre@gmail.com>
2014-12-15 04:28:55 +0100

    added most functions 	modified:   src/PrimeSieve.jl

--- src/PrimeSieve.jl
+++ src/PrimeSieve.jl
@@ -3,13 +3,41 @@ module PrimeSieve
 include("../deps/deps.jl")
 import Base: convert, ccall
 
-export countprimes, primes, nprimes, nthprime, pnthprime, primetest
+export primes, nprimes, nthprime, pnthprime
+export countprimes, pcountprimes
+export countprimes2, pcountprimes2
+export countprimes3, pcountprimes3
+export countprimes4, pcountprimes4
+export countprimes5, pcountprimes5
+export countprimes6, pcountprimes6
+export primetest
 
 ##
 
+const stoplimit = uint64(2)^64 - uint64(2)^32 * uint64(10)
+const startlimit = uint64(2)^64 - uint64(2)^32 * uint64(11)
+
+checkstop(val) = val <= stoplimit ? true : error("stop value ", val, " is greater than limit: ", val)
+checkstart(val) = val <= startlimit ? true : error("start value ", val, " is greater than limit: ", val)
+
 # Returned primes have this data type
+const SHORT_PRIMES = 0
+const USHORT_PRIMES = 1
+const INT_PRIMES = 2
+const UINT_PRIMES = 3
 const LONG_PRIMES = 4
 const ULONG_PRIMES = 5
+const LONGLONG_PRIMES = 6
+const ULONGLONG_PRIMES = 7
+
+for (ctype,typecode) in ((:Cshort, :SHORT_PRIMES),(:Cushort, :USHORT_PRIMES),
+                         (:Cint, :INT_PRIMES),(:Cuint, :UINT_PRIMES),
+                         (:Clong, :LONG_PRIMES),(:Culong, :ULONG_PRIMES),
+                         (:Clonglong, :LONGLONG_PRIMES),(:Culonglong, :ULONGLONG_PRIMES))
+    @eval begin
+        primetype(::Type{$ctype}) = $typecode
+    end
+end
 
 const libname = "primesieve/.libs/libprimesieve.so.4"
 
@@ -21,36 +49,22 @@ function primescopy(res,n)
     nretv
 end
 
-function countprimes{T,V}(start::T, stop::V)
-    (start,stop) = promote(start,stop)
-    res = ccall((:primesieve_count_primes,libname),
-                Ptr{Uint64}, (Uint64, Uint64),
-                convert(Uint64,start), convert(Uint64,stop))
-    convert(typeof(start),res)
-end
-
-countprimes(stop) = countprimes(one(typeof(stop)),stop)
-
-primetype(::Type{Int64}) = LONG_PRIMES
-primetype(::Type{Uint64}) = ULONG_PRIMES
-
 function primes{T,V}(start::T,stop::V)
-    (start,stop) = promote(start,stop)
-    NT = typeof(start)
+    checkstop(stop)
     n = Csize_t[0]
     res = ccall((:primesieve_generate_primes, libname),
                 Ptr{T}, (Uint64, Uint64, Ptr{Csize_t}, Int),
-                convert(Uint64,start),convert(Uint64,stop),n,primetype(typeof(start)))
+                convert(Uint64,start),convert(Uint64,stop),n,primetype(T))
     primescopy(res,n[1])
 end
 
 primes(stop) = primes(one(typeof(stop)),stop)
 
-function nprimes(nin,start)
-    n = convert(Int,nin)
+function nprimes{T}(n::T,start)
+    checkstop(start) # not sure what he means here
     res = ccall((:primesieve_generate_n_primes, libname),
-                Ptr{Int64}, (Uint64, Uint64, Int),
-                convert(Uint64,n),convert(Uint64,start),LONG_PRIMES)
+                Ptr{T}, (Uint64, Uint64, Int),
+                convert(Uint64,n),convert(Uint64,start),primetype(T))
     primescopy(res,n)
 end
 
@@ -63,13 +77,41 @@ nprimes(n) = nprimes(n,one(typeof(n)))
 for (cname,jname) in ((:(:primesieve_nth_prime), :nthprime),
                       (:(:primesieve_parallel_nth_prime), :pnthprime))
     @eval begin
-        function ($jname)(n,start)
+        function ($jname){T}(n,start::T)
+            checkstart(start)
             res = ccall(( $cname, libname),
                          Ptr{Uint64}, (Int64, Uint64),
-                        convert(Uint64,n),convert(Int64,start))
-            convert(Int,res)
+                        convert(Int64,n),convert(Uint64,start))
+            convert(T,res)
         end
-        ($jname)(n) = ($jname)(n,one(typeof(n)))        
+        ($jname)(n) = ($jname)(n,1)
+    end
+end
+
+for (cname,jname) in (
+                      (:(:primesieve_count_primes), :countprimes),
+                      (:(:primesieve_count_twins), :countprimes2),
+                      (:(:primesieve_count_triplets), :countprimes3),
+                      (:(:primesieve_count_quadruplets), :countprimes4),
+                      (:(:primesieve_count_quintuplets), :countprimes5),
+                      (:(:primesieve_count_sextuplets), :countprimes6),
+
+                      (:(:primesieve_parallel_count_primes), :pcountprimes),
+                      (:(:primesieve_parallel_count_twins), :pcountprimes2),
+                      (:(:primesieve_parallel_count_triplets), :pcountprimes3),
+                      (:(:primesieve_parallel_count_quadruplets), :pcountprimes4),
+                      (:(:primesieve_parallel_count_quintuplets), :pcountprimes5),
+                      (:(:primesieve_parallel_count_sextuplets), :pcountprimes6))
+    @eval begin
+        function ($jname){T,V}(start::T, stop::V)
+            checkstop(stop)
+            (start,stop) = promote(start,stop)
+            res = ccall(($cname,libname),
+                        Ptr{Uint64}, (Uint64, Uint64),
+                        convert(Uint64,start), convert(Uint64,stop))
+            convert(typeof(start),res)
+        end
+        ($jname)(stop) = ($jname)(one(typeof(stop)),stop)
     end
 end
 
