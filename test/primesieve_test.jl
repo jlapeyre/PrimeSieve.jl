3bcdc3fd7f4671a13d27e037e96c381bb1eb1fa4
John Lapeyre <john.lapeyre@gmail.com>
2014-12-30 12:56:31 +0100

    reinstate all tests. segfault mysteriously disappeared 	modified:   test/primesieve_test.jl

--- test/primesieve_test.jl
+++ test/primesieve_test.jl
@@ -1,12 +1,12 @@
 # This loop was causing all memory to be allocated. It stopped,
 # but all I did was add a println and then remove it.
-# for (typef, ptype) in ((:int32, :Int32), (:int64 ,:Int64), (:uint64 ,:Uint64))
-#     @eval begin
-#         p = genprimes($typef(1000))
-#         @test length(p) == 168
-#         @test eltype(p) == $ptype 
-#     end
-# end
+for (typef, ptype) in ((:int32, :Int32), (:int64 ,:Int64), (:uint64 ,:Uint64))
+     @eval begin
+         p = genprimes($typef(1000))
+         @test length(p) == 168
+         @test eltype(p) == $ptype 
+     end
+end
 
 
 a = 24238423
@@ -14,12 +14,12 @@ b = 73923403
 c = 100000
 
 @test countprimes(b) == ntcountprimes(b)
-#@test typeof(genprimes("10")[1]) == Uint64
-#@test countprimes(a,b) == ntcountprimes(a,b)
-#@test genprimes(c) == Base.primes(c)
-#@test typeof(nprimes("10")[1]) == Uint64
-#@test ntcountprimes("10^9") == 50847534
+@test typeof(genprimes("10")[1]) == Uint64
+@test countprimes(a,b) == ntcountprimes(a,b)
+@test genprimes(c) == Base.primes(c)
+@test typeof(nprimes("10")[1]) == Uint64
+@test ntcountprimes("10^9") == 50847534
 
-# @test countprimes(:(10^19)) == 234057667276344607
-# @test countprimes("10^20") == 2220819602560918840
-# @test typeof(primelookup("2^63")) == (Int64,(Int128,Int128,Int128))
+@test countprimes(:(10^19)) == 234057667276344607
+@test countprimes("10^20") == 2220819602560918840
+@test typeof(primelookup("2^63")) == (Int64,(Int128,Int128,Int128))
