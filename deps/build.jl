c1701c22662fad3d3b800b16cca3a23060a5ca81
John Lapeyre <john.lapeyre@gmail.com>
2014-12-30 22:54:35 +0100

    clean build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -4,7 +4,7 @@ using BinDeps
 julialibpath = dirname(Sys.dlpath(dlopen("libgmp")))
 
 # Set link flags for Autotools external packages
-# This is only needed for ecm
+# This is only needed for the ecm package
 ENV["LDFLAGS"] = "-L$julialibpath"
 # config.log shows that BinDeps also set the following as well.
 # We have copied gmp.h from Julia source tree to this location. Maybe Julia should also copy it to installation.
@@ -12,7 +12,7 @@ ENV["CPPFLAGS"] = "-I../../usr/include"
 
 # The  -lgmp should not be neccessary, but it is.
 # -lgmp occurs multiple times on link commands. -lm occurs five times per link command.
-# -Wl... makes the libecm use libgmp in the Julia installation.
+# -Wl... makes the libecm use libgmp in the Julia installation rather than the system libgmp.
 ENV["LIBS"] = "-lgmp -Wl,-rpath -Wl,$julialibpath"
 
 include("../src/compatibility.jl")
@@ -25,18 +25,17 @@ primecount = library_dependency("primecount", aliases = ["libprimecount"], depen
 cprimecount = library_dependency("cprimecount", aliases = ["libcprimecount"], depends = [primecount])
 smsieve = library_dependency("smsieve", aliases = ["libsmsieve"], depends = [gmpecm])
 
-#const pkgdir = BinDeps.depsdir(cprimecount)
-
 provides(Sources, URI("http://dl.bintray.com/kimwalisch/primesieve/primesieve-5.4.1.tar.gz"), primesieve)
 provides(Sources, URI("http://dl.bintray.com/kimwalisch/primecount/primecount-1.4.tar.gz"), primecount)
 provides(Sources, URI("https://gforge.inria.fr/frs/download.php/file/32159/ecm-6.4.4.tar.gz"), gmpecm)
 provides(Sources, URI("https://github.com/jlapeyre/msieve-shared/archive/v0.0.3.tar.gz"), smsieve,unpacked_dir="msieve-shared-0.0.3")
 
-provides(BuildProcess, Autotools(libtarget = ".libs/libecm."*BinDeps.shlib_ext, configure_options = String["--enable-shared", "--enable-openmp","--with-gmp-lib=$julialibpath"
-                                 ]), gmpecm)
+provides(BuildProcess, Autotools(libtarget = ".libs/libecm."*BinDeps.shlib_ext, configure_options =
+                                 String["--enable-shared", "--enable-openmp","--with-gmp-lib=$julialibpath"]), gmpecm)
 provides(BuildProcess, Autotools(libtarget = ".libs/libprimesieve."*BinDeps.shlib_ext), primesieve)
 provides(BuildProcess, Autotools(libtarget = ".libs/libprimecount."*BinDeps.shlib_ext), primecount)
 
+# BinDeps.depsdir(cprimecount) is /pathto/PackageName/deps
 const cpcsrcdir = joinpath(BinDeps.depsdir(cprimecount),"src","cprimecount")
 provides(SimpleBuild,
     (@build_steps begin
