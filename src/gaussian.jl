6e93bf6c970b558dab9c04de7041fac8d54cf31a
John Lapeyre <john.lapeyre@gmail.com>
2015-01-05 22:41:03 +0100

    use abs2 instead of doing it by hand

--- src/gaussian.jl
+++ src/gaussian.jl
@@ -5,5 +5,5 @@ function isprime{T<:Integer}(z::Complex{T})
     (x,y) = (real(z),imag(z))
     x == 0 && isprime(y) && y % 4 == 3 && return true
     y == 0 && isprime(x) && x % 4 == 3 && return true
-    isprime(x*x+y*y)
+    isprime(abs2(z))
 end
